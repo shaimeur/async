4efefe08dae5940a6c8dd3d02e347390cdb6acb3
shaimeur <haimeursaad@gmail.com>
2024-03-22 23:34:38 +0000

    replacing the callbacks with the then and catch method with promises the then methods is used when we a promise i fullfuld and the catch method is used when an error occure but its still not perfect

--- index.js
+++ index.js
@@ -6,16 +6,15 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) {
-        return console.log('this is an error!!=>', err.message);
-      }
-      //console.log(res.body.message);
+    .then((res) => {
       let result = res.body.message;
 
       fs.writeFile(`${__dirname}/dog-image.txt`, result, (err) => {
         if (err) return console.log(err.message);
         console.log('message writenn succefully!!');
       });
+    })
+    .catch((err) => {
+      console.log('this is an error!!=>', err.message);
     });
 });
